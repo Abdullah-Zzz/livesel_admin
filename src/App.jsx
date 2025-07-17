48d24210972f49339f4d15e1e56683b5e782d8d8
abdullah-zzz <ak0344494@gmail.com>
2025-07-17 19:36:02 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
         <Route path="/admin/login" element={<AdminLogin />} />
 
         <Route
-          path="/admin"
+          path="/"
           element={
             <RequireAdmin>
               <AdminLayout />
